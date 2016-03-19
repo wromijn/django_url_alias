57061c542f12b5b4fd675cac67281e67541afcec
Willem Romijn <wromijn@gmail.com>
2016-03-19 12:30:45 +0100

    Update utils.py

--- django_url_alias/utils.py
+++ django_url_alias/utils.py
@@ -1,6 +1,11 @@
 from django.core.urlresolvers import get_script_prefix
 from django.utils import six
-from django.utils.importlib import import_module
+try:
+    # Django versions >= 1.9
+    from django.utils.module_loading import import_module
+except ImportError:
+    # Django versions < 1.9
+    from django.utils.importlib import import_module
 from .aliases import default_url_alias_modules
 
 
