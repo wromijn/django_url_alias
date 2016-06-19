59c9495c2193c83f2f4dd701373df6fe7852bb9f
Willem Romijn <wromijn@gmail.com>
2016-06-19 10:18:59 +0200

    Update urls.py

--- django_url_alias/urls.py
+++ django_url_alias/urls.py
@@ -1,8 +1,5 @@
-from django.conf.urls import patterns, include, url
 from django_url_alias.utils import aliased_urls
 from .settings import URL_ALIAS_ROOT_URLCONF
 
 
-urlpatterns = patterns('',
-    aliased_urls(URL_ALIAS_ROOT_URLCONF),
-)
+urlpatterns = [aliased_urls(URL_ALIAS_ROOT_URLCONF)]
